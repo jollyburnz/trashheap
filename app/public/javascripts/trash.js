766ef8256b2c317472b32262d3097c248c289e20
James Andino <totheleftpanda@gmail.com>
2012-03-05 17:00:37 -0500

    basic navigation controls for Ipad

--- app/public/javascripts/trash.js
+++ app/public/javascripts/trash.js
@@ -16,21 +16,113 @@ $(function(){
 	    },
 	  false
 	);
+	
+	Slider = function(main,slides){
+		this.slides = main.concat(slides);
+		this.current = 0;
+
+		this.lock = false;
+
+		this.default_animation = 'dissolve';
+
+	}
+
+		Slider.prototype.left = function(animation){
+			if(this.is_locked()){return}
+			animation = animation || this.default_animation;
+
+			if(!this.current){
+				this.current = this.slides.length;
+			}else{
+				this.current = this.current - 1 ;
+			}
+				jQT.goTo($(this.slides[this.current]),animation);
+
+		}
+
+		Slider.prototype.right = function(animation){
+			if(this.is_locked()){return}
+			animation = animation || this.default_animation;
+
+			if(this.current + 1 > this.slides.length){
+				this.current = 0
+			}else{
+				this.current = this.current + 1 ;
+			}
+				jQT.goTo($(this.slides[this.current]),animation);	
+
+		}
+
+		Slider.prototype.on_extra = false;
+
+
+		Slider.prototype.extra_go = function(animation){
+			this.lock = true;
+			animation = animation || this.default_animation;
+				if(!this.on_extra){
+					jQT.goTo('#extra',animation);
+					this.on_extra = !this.on_extra;
+				}
+		}
+
+		Slider.prototype.extra_leave = function(animation){
+			this.lock = false;
+			animation = animation || this.default_animation;
+				if(this.on_extra){
+					jQT.goTo($(this.slides[this.current]),animation);
+					this.on_extra = !this.on_extra;
+				}
+		}
+
+		Slider.prototype.home = function(animation){
+			animation = animation || this.default_animation;
+			
+			jQT.goTo($(this.slides[0]),animation);
+			this.lock = false;
+		}
+
+		Slider.prototype.goTo = function (name,animation){
+			animation = animation || this.default_animation;
+			
+			jQT.goTo(name,animation);
+			this.lock = false;
+		}
+
+
+		Slider.prototype.is_locked = function(){
+			return this.lock;
+		}
+
+		Slider.prototype.lock_for_orientation = function(orientation){
+		
+		}
+
+
+
+	// var $main $slides Slider slider
+
+	$main	= $('#main');
+	$slides = $('.slide');
+
+
+	slider = new Slider($main,$slides);
 
 	
 	$('#jqt').swipeRight(function(evt,data){
-	
+		slider.left('slideright');
 	});
 	
 	$('#jqt').swipeLeft(function(evt,data){
-	
+		slider.right('slideleft');
+	});
+
+	$('#jqt').swipeUp(function(evt,data){
+		slider.extra_leave('slideup');
 	});
 
 	$('#jqt').swipeDown(function(evt,data){
-	
+		slider.extra_go('pop');
 	});
-
-
 
 
 
