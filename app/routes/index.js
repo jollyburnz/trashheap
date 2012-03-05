1dbef38bd67c2daf7c3a7c66d211d47de9a26fe1
Rumple Fraggle <rumplefraggle@gmail.com>
2012-03-05 13:46:29 -0500

    added yaml options file loaded every time

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,7 +8,8 @@ var fs = require('fs');
 
 exports.index = function(req, res){
 
-var merge = function(obj_1,obj_2){
+var merge = function(obj_1,obj_2){	
+
 	for(var key in obj_2){
 		obj_1[key] = obj_2[key];
 	}
@@ -19,31 +20,39 @@ var merge = function(obj_1,obj_2){
 		content.title = 'H.art';
 
 
-
-	if(req.params.yaml){
-		var file = 'yaml/'+req.params.yaml+'.yaml';
-
-		fs.stat(file,function(err,stats){
-			if(!err){
-				if(stats.isFile()){
-					fs.readFile(file,'utf-8',function(err,file_contents){
-						if(!err){
-							content = merge(content,yaml.load(file_contents)); 
-							res.render('index',{"content":content});	
-						}else{
-							res.render('index',{"content":{}});
-							console.log(err);
-						}
-					});
-				}else{
-					res.render('index',{"content":{}});
-				}
-			}else{	
-			res.render('index',{"content":{}});
+	fs.readFile('options.yaml','utf-8',function(err,file_contents){
+		if(!err){
+			content = merge(content,yaml.load(file_contents));
+		}else{
 			console.log(err);
-			}
-		});
-	}else{
-	res.render('index',{"content":{}});
-	}
+		}
+
+		if(req.params.yaml){
+			var file = 'yaml/'+req.params.yaml+'.yaml';
+
+			fs.stat(file,function(err,stats){
+				if(!err){
+					if(stats.isFile()){
+						fs.readFile(file,'utf-8',function(err,file_contents){
+							if(!err){
+								content = merge(content,yaml.load(file_contents)); 
+								res.render('index',{"content":content});	
+							}else{
+								res.render('index',{"content":content});
+								console.log(err);
+							}
+						});
+					}else{
+						res.render('index',{"content":content});
+					}
+				}else{	
+				res.render('index',{"content":content});
+				console.log(err);
+				}
+			});
+		}else{
+		res.render('index',{"content":content});
+		}
+
+	});
 };
